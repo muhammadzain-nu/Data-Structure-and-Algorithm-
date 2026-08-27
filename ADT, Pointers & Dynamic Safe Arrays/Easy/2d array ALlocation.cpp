fd09359b4db8f3a8c10d87c372588a298ea953c4
Muhammad Zain <k253050@nu.edu.pk>
2026-08-27 03:10:13 -0700

    Add files via upload

---
+++ ADT, Pointers & Dynamic Safe Arrays/Easy/2d array ALlocation.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int row, col;
+	
+	cout<<"Enter rows"<< endl;
+	cin>>row;
+cout<<"Enter columns"<< endl ;
+cin>>col;
+int **matrix= new int*[row];
+for(int i=0;i<col;i++){
+	matrix[i] = new int[col];
+}
+cout<<"Memory ALlocated"<< endl;
+//input function
+for(int i=0;i<row;i++){
+	for(int j=0;j<col;j++){
+		cout<<"Enter the element of row:"<< i << "and column:"<< j <<endl;
+		cin>>matrix[i][j];
+	}
+}
+//display function
+	for(int i=0;i<row;i++){
+	for(int j=0;j<col;j++){
+		cout<<matrix[i][j];
+	}
+	cout<<"\n";
+}
+	return 0;
+}
